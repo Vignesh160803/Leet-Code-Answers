6bb64477dc1aacfb4c494df7e70b3d0371518677
Vignesh160803 <vig160803@gmail.com>
2024-07-12 19:30:05 +0530

    leet-code-75

---
+++ Leet-Code-75/MergeStringsAlternatively.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        lptr=0
+        rptr=0
+        n=len(word1)
+        m=len(word2)
+        res=""
+        while lptr<n and rptr<m:
+            res+=word1[lptr]+word2[rptr]
+            lptr+=1
+            rptr+=1
+        while lptr<n:
+            res+=word1[lptr]
+            lptr+=1
+        while rptr<m:
+            res+=word2[rptr]
+            rptr+=1
+
+        return res
